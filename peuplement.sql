f33a75f1896ee6737467dff5aecb2360d6cdbda5
Shuudy <ancieux.jerome059@gmail.com>
2025-01-27 14:09:49 +0100

    feat: add initial data population for objets, pieces, and personnages

---
+++ peuplement.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO objets (nom) VALUES
+    ('Couteau'),
+    ('Chandelier'),
+    ('Revolver'),
+    ('Corde'),
+    ('Matraque'),
+    ('Clé anglaise');
+
+INSERT INTO pieces (nom) VALUES
+    ('Cuisine'),
+    ('Salle de bal'),
+    ('Véranda'),
+    ('Bibliothèque'),
+    ('Bureau'),
+    ('Salle à manger'),
+    ('Hall'),
+    ('Salon'),
+    ('Salle de billard');
+
+INSERT INTO personnages (nom, couleur) VALUES
+    ('Mademoiselle Rose', 'Rose'),
+    ('Colonel Moutarde', 'Jaune'),
+    ('Madame Leblanc', 'Blanc'),
+    ('Révérend Olive', 'Vert'),
+    ('Professeur Violet', 'Violet'),
+    ('Madame Pervenche', 'Bleu');
